b34ff81cc27a6ef736a3b88d0729df1f8312b566
Gustavo Coutinho <gustavocoutinho32@gmail.com>
2026-04-08 15:21:35 -0300

    atualização

--- Site/src/components/ScrollToTop.jsx
+++ Site/src/components/ScrollToTop.jsx
@@ -5,6 +5,10 @@ const ScrollToTop = () => {
     const { pathname } = useLocation();
 
     useEffect(() => {
+        // Previne que o navegador tente restaurar a posição do scroll ao atualizar a página (F5)
+        if ('scrollRestoration' in window.history) {
+            window.history.scrollRestoration = 'manual';
+        }
         window.scrollTo(0, 0);
     }, [pathname]);
 
